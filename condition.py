44cd81b913e5c803c38deb8baf8e04f3edeab0fb
Manoj Das <manoj@vrittechnologies.com>
2025-08-18 08:30:28 +0545

    condition

---
+++ condition.py
@@ -0,0 +1,19 @@
+# is_nepali_citizen = True
+
+# if is_nepali_citizen:
+#    print("you can vote in nepal.")
+
+
+# age = 19
+
+# if age>18:
+#    print("you can Vote")
+
+#WAP to print "You can Vote" if he/she is nepali citizen and age is greater and equal to 20
+is_nepali_citizen = True
+age = 17
+
+if is_nepali_citizen == True and 17 >= 20:
+    print("you can vote")
+
+#Let suppose your Facebook password is "skillshikshya" , if password is match with user input password then print "You are login successfully !!!"
