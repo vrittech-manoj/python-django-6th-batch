7d669712064a1b119d146ea564b0c914c675cfff
Manoj Das <manoj@vrittechnologies.com>
2025-08-19 08:16:58 +0545

    palidrome

---
+++ if_condition.py
@@ -0,0 +1,6 @@
+user_input_password = input("Enter Your Password#>>>")
+
+if user_input_password == "skillshikshya":
+    print("You are login successfully !")
+else:
+    print("Sorry your password is incorrect !!!")
---
+++ odd_even.py
@@ -0,0 +1,22 @@
+# #WAP to find out a given number is odd or even. if odd print "given number is odd" if even print "Even"
+# odd = number / 2 = remainder = 1 -> odd else even(0)
+# num%2
+
+# #WAP to check given word is palindrome or not(LEVEL)(reverse)
+
+# step-1: define any word
+# step-2: reverse the word 
+# step-3: compare both word (define word,reverse word)
+# step-4: if compare true print "given word is palindrom"
+# step-5: else print "given word is not palindrom"
+
+word = "APPLE"
+reverse_word = word[::-1]
+if word == reverse_word:
+    print(f"the given word {word} is palindrome word")
+else:
+    print(word, " is not palindrome")
+
+
+
+
